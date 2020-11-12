6af444545f8ea6fb428dc9012b2e4a89005cb67a
petermeissner <retep.meissner@gmail.com>
2020-11-12 22:11:40 +0100

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from pdfdata import *
+from pprint import pprint
+
+# parse pdf as dictionary
+pdf_parsed = parse_pdf('pdfs/0641-20.pdf')
+res        = pdf_doc_extract_span_list(pdf_parsed)
+
+pprint(res, depth=3)
+
+# parse pdf as list of spans
+pdf_parsed = parse_pdf('pdfs/0641-20.pdf')
+res        = pdf_doc_extract_span_df(pdf_parsed)
+
+pprint(res[0])
+
+# transform pdf text to jsonnl
+pdf_text_to_jsonnl('pdfs/0641-20.pdf', '0641-20.jsonnl')
